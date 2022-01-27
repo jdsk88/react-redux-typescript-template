d58625a9627a6907ca99c0f800621875ba9280a5
jdsk88 <jdsk88@gmail.com>
2022-01-27 21:11:56 +0100

    router with outlet

--- src/layouts/main/index.tsx
+++ src/layouts/main/index.tsx
@@ -6,7 +6,7 @@ const MainLayout: React.FC = (): JSX.Element => {
     <>
       <nav>
         <ul>
-          <li><Link to='/'>Main Page</Link></li>
+          <li><Link to='/agdagda'>Main Page</Link></li>
           <li><Link to='/account/add'>Add Account</Link></li>
           <li><Link to='/account/list'>List Accounts</Link></li>
           <li><Link to='/account/1'>View Account</Link></li>
